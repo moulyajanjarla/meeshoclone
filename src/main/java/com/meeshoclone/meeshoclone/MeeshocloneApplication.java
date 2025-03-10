24660050a33faa4fac4210ad5d844c8199c62356
moulyajanjarla <106820382+moulyajanjarla@users.noreply.github.com>
2025-03-10 14:07:55 +0530

    first commit

---
+++ src/main/java/com/meeshoclone/meeshoclone/MeeshocloneApplication.java
@@ -0,0 +1,13 @@
+package com.meeshoclone.meeshoclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeeshocloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeeshocloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meeshoclone/meeshoclone/MeeshocloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meeshoclone.meeshoclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeeshocloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
